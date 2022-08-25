92231c8ebce1a58074356b11ff049a6988f554fe
jungcheolkwon <o2man@hanmail.net>
2022-08-26 01:39:14 +0800

    updated

--- ctrl.sh
+++ ctrl.sh
@@ -2,16 +2,15 @@
 
 #edited by JC
 #j.kwon@f5.com
-# https://clouddocs.f5.com/api/velos-api/api-workflows.html 
+# https://clouddocs.f5.com/api/velos-api/api-workflows.html
 
 #change to your username
-name="your-username"
+name="username"
 #change the password for your user
-password="xxxxxxxxxxxxx"
+password="password"
 
 echo -e "\033[35mType your rSeries IPaddress or hostname: \033[0m"
 read ip
-
 token=$(curl -sk -u "$name":"$password" -H "Content-Type: application/yang-data+json" -H "X-Auth-Token: rctoken" -X GET https://$ip:8888/restconf/data/openconfig-system:system/aaa | jq -r .[].'"f5-primary-key:primary-key"'.state.hash)
 
 if [[ ( "$1" ==  "get" ) && ( ( $2 == 'ntp' ) || ( $2 == 'dns' ) || ( $2 == 'clock' ) )  ]]
@@ -25,7 +24,6 @@ then
 
 elif [[ ( "$1" ==  "get" ) && ( $2 == 'tenant' ) ]]
 then
-  #tenant
   curl -sk -u "$name":"$password" -H "Content-Type: application/yang-data+json" -H "X-Auth-Token: rctoken" -X GET https://$ip:8888/restconf/data/f5-tenants:tenants | jq -r .[].tenant
 
 elif [[ ( "$1" ==  "create" ) && ( $2 == 'dns' ) ]]
@@ -42,17 +40,26 @@ then
 
 elif [[ ( "$1" ==  "create" ) && ( $2 == 'tenant' ) ]]
 then
-  ##POST Loop
   curl -sk -u "$name":"$password" -H "Content-Type: application/yang-data+json" -X POST -d @tenant https://$ip:8888/restconf/data/f5-tenants:tenants
 
 elif [[ ( "$1" ==  "delete" ) && ( $2 == 'tenant' ) ]]
 then
-  del="$3"
-  curl -sk -u "$name":"$password" -H "Content-Type: application/yang-data+json" -H "X-Auth-Token: $token" -X DELETD https://$ip:8888/restconf/data/f5-tenants:tenants/tenant=$3
+  curl -sk -u "$name":"$password" -H "Content-Type: application/yang-data+json" -H "X-Auth-Token: $token" -X DELETE https://$ip:8888/restconf/data/f5-tenants:tenants/tenant=$3
+
+elif [[ ( "$1" ==  "upload" ) && ( $2 == 'system' ) ]]
+then
+  echo -e "\033[35mType system image name: \033[0m"
+  read image
+  scp ISO/$image root@$ip:/var/import/staging/
+
+elif [[ ( "$1" ==  "upload" ) && ( $2 == 'tenant' ) ]]
+then
+  echo -e "\033[35mType tenant image name: \033[0m"
+  read image
+  scp ISO/$image root@$ip:/var/F5/system/IMAGES
 
 elif [ "$1" ==  "change" ]
 then
   host="$2"
   curl -sk -u "$name:$password" -H "Content-Type: application/yang-data+json" -X POST -d @pwd https://$host:8888/restconf/operations/openconfig-system:system/aaa/authentication/users/user=admin/config/change-password
-
 fi
